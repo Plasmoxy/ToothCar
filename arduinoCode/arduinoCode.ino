631539b75be6e742abee63007e8706b1540e6dd5
Plasmoxy <31391787+Plasmoxy@users.noreply.github.com>
2018-01-15 08:54:31 +0100

    progressive turning

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -9,11 +9,12 @@ const byte backwardPin = 6;
 const byte servoPin = 3;
 const byte blueRX = 2;
 const byte servoMin = 42;
-const byte servoMax = 85;
-const byte servoStraight = (servoMin+servoMax)/2 - 3;
+const byte servoMax = 83;
+const byte directionChange = 20;
 
 char cmd = 0;
 boolean blueConnected = false;
+int direct = 0;
 
 Servo servo;
 SoftwareSerial blue(blueRX, 16); // 16 doesnt exist, no tx
@@ -23,7 +24,7 @@ void setup() {
   pinMode(forwardPin, 1);
   pinMode(backwardPin, 1);
   servo.attach(servoPin);
-  straight();
+  turn(0);
   stopp();
 
   beep();beep(); 
@@ -40,31 +41,33 @@ void loop() {
     cmd = char(blue.read());
 
     if (cmd=='F') {
-      straight();
+      addStraight();
       forward();
     } else if(cmd=='B') {
-      straight();
+      addStraight();
       backward();
     } else if(cmd=='L') {
-      left();
+      addLeft();
     } else if(cmd=='R') {
-      right();
+      addRight();
     } else if(cmd=='G') {
-      left();
+      addLeft();
       forward();
     } else if(cmd=='I') {
-      right();
+      addRight();
       forward();
     } else if(cmd=='H') {
-      left();
+      addLeft();
       backward();
     } else if(cmd=='J') {
-      right();
+      addRight();
       backward();
     } else if(cmd=='S') {
-      straight();
+      addStraight();
       stopp();
     }
+
+    updateDirection();
   }
 }
 
@@ -87,16 +90,25 @@ void superBeep() {
   digitalWrite(forwardPin, 0);
 }
 
-void left() {
-  servo.write(servoMax);
+void updateDirection() {
+  turn(direct);
 }
 
-void right() {
-  servo.write(servoMin);
+void turn(int pos) {
+  servo.write(map(pos, 100, -100, servoMin, servoMax));
 }
 
-void straight() {
-  servo.write(servoStraight);
+void addLeft() {
+  if (direct > -100) direct -= directionChange;
+}
+
+void addRight() {
+  if (direct < 100) direct += directionChange;
+}
+
+void addStraight() {
+  if (direct > 0) direct -= directionChange;
+  if (direct < 0) direct += directionChange;
 }
 
 void forward() {
